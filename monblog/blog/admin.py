4743f464e35966172bd6e4a165f9820a7ce10698
babacar Ndiaye <babacarndiaye808@gmail.com>
2025-04-03 01:21:18 +0000

    créations superutilisateur avec ses identifiants

--- monblog/blog/admin.py
+++ monblog/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'auteur', 'date_publication')  # Colonnes affichées dans l'admin
+    search_fields = ('titre', 'auteur')  # Barre de recherche par titre/auteur

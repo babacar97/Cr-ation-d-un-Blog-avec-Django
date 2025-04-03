0db95a832f87f842096db38c775e56b800ace4db
babacar Ndiaye <babacarndiaye808@gmail.com>
2025-04-03 01:17:33 +0000

    la structure du BD est ajouté ainci  que  les migrations

--- monblog/blog/models.py
+++ monblog/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    titre = models.CharField(max_length=200)  # Titre de l'article
+    contenu = models.TextField()  # Contenu de l'article
+    date_publication = models.DateTimeField(auto_now_add=True)  # Date automatique
+    auteur = models.CharField(max_length=100)  # Auteur de l'article
+    image = models.ImageField(upload_to='articles/', blank=True, null=True)  # Image facultative
+
+    def __str__(self):
+        return self.titre  # Affiche le titre dans l'admin Django
